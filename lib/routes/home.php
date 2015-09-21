57d9c95adfa1c51cf088cbe080a66b65e66031a8
Marco Ardizzone <kwbmm1990@gmail.com>
2015-09-21 11:32:39 +0200

    Home should now display proper scores

--- lib/routes/home.php
+++ lib/routes/home.php
@@ -1,19 +1,26 @@
 <?php 
   $app->get('/home',function() use($app){
     $now = time();
-    $query =  "SELECT username, points FROM user, scores
-              WHERE user.UID = scores.UID
-              AND scores.MID = (
-                SELECT MAX(MID) FROM match_day
-                WHERE match_day.end <= '$now')
-              ORDER BY points DESC";
+    $i=0;
+    do{
+      $query =  "SELECT scores.MID as MID, username, points FROM user, scores
+                WHERE user.UID = scores.UID
+                AND scores.MID = (
+                  SELECT MAX(MID)-'$i' FROM match_day
+                  WHERE match_day.end <= '$now')
+                ORDER BY points DESC";
 
-    $result = getResult($app['conn'],$query);
-    if($result === false)
-      $app->abort(452,__FILE__." (".__LINE__.")");
-    $sequence = array();
-    while(($row=mysqli_fetch_array($result,MYSQLI_ASSOC))!== null)
-      array_push($sequence, $row);
+      $result = getResult($app['conn'],$query);
+      if($result === false)
+        $app->abort(452,__FILE__." (".__LINE__.")");
+      $sequence = array();
+      while(($row=mysqli_fetch_array($result,MYSQLI_ASSOC))!== null){
+        $mid = $row['MID']; //Save the match ID
+        unset($row['MID']); //Remove it from the row
+        array_push($sequence, $row); //Put the row in the result to display
+      }
+      $i++;
+    }while(empty($sequence) && !isset($mid));
     $twigParameters = getTwigParameters('Home',$app['siteName'],'home',$app['userMoney'],array('sequence'=>$sequence));
     return $app['twig']->render('index.twig',$twigParameters);
   });
