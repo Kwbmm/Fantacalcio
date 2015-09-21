c41fba94fb924e44d58e25a0d8c7de33f1606105
Marco Ardizzone <kwbmm1990@gmail.com>
2015-09-21 18:49:23 +0200

    Now it's fixed..

--- lib/routes/compute_results.php
+++ lib/routes/compute_results.php
@@ -66,8 +66,6 @@
 
         foreach ($notPlayed as $role => $mark)
           $marks[$role] = (float)0;
-
-        myDump($marks);
         $total = (float)0;
         if(isset($marks)){ //$marks is set only if user made a formation
           if($marks['POR'] === (float)0 ){
@@ -76,218 +74,188 @@
           }
           else
             $total += $marks['POR'];
-          myDump("POR");
-          myDump($total);
 
           if($marks['DIF-1'] === (float)0){
-            if(isset($marks['DIF-R-1']) && $marks['DIF-R-1'] !== 0 ){
+            if(isset($marks['DIF-R-1']) && $marks['DIF-R-1'] !== (float)0 ){
               $total += $marks['DIF-R-1'];
               unset($marks['DIF-R-1']);
             }
-            elseif(isset($marks['DIF-R-2']) && $marks['DIF-R-2'] !== 0){
+            elseif(isset($marks['DIF-R-2']) && $marks['DIF-R-2'] !== (float)0){
               $total += $marks['DIF-R-2'];
               unset($marks['DIF-R-2']);
             }
           }
           else
             $total += $marks['DIF-1'];
-          myDump("DIF-1");
-          myDump($total);
 
           if($marks['DIF-2'] === (float)0){
-            if(isset($marks['DIF-R-1']) && $marks['DIF-R-1'] !== 0 ){
+            if(isset($marks['DIF-R-1']) && $marks['DIF-R-1'] !== (float)0 ){
               $total += $marks['DIF-R-1'];
               unset($marks['DIF-R-1']);
             }
-            elseif(isset($marks['DIF-R-2']) && $marks['DIF-R-2'] !== 0){
+            elseif(isset($marks['DIF-R-2']) && $marks['DIF-R-2'] !== (float)0){
               $total += $marks['DIF-R-2'];
               unset($marks['DIF-R-2']);
             }
           }
           else
             $total += $marks['DIF-2'];
-          myDump("DIF-2");
-          myDump($total);
 
           if($marks['DIF-3'] === (float)0){
-            if(isset($marks['DIF-R-1']) && $marks['DIF-R-1'] !== 0 ){
+            if(isset($marks['DIF-R-1']) && $marks['DIF-R-1'] !== (float)0 ){
               $total += $marks['DIF-R-1'];
               unset($marks['DIF-R-1']);
             }
-            elseif(isset($marks['DIF-R-2']) && $marks['DIF-R-2'] !== 0){
+            elseif(isset($marks['DIF-R-2']) && $marks['DIF-R-2'] !== (float)0){
               $total += $marks['DIF-R-2'];
               unset($marks['DIF-R-2']);
             }
           }
           else
             $total += $marks['DIF-3'];
-          myDump("DIF-3");
-          myDump($total);
 
           if(isset($marks['DIF-4'])){
             if($marks['DIF-4'] === (float)0){
-              if(isset($marks['DIF-R-1']) && $marks['DIF-R-1'] !== 0 ){
+              if(isset($marks['DIF-R-1']) && $marks['DIF-R-1'] !== (float)0 ){
                 $total += $marks['DIF-R-1'];
                 unset($marks['DIF-R-1']);
               }
-              elseif(isset($marks['DIF-R-2']) && $marks['DIF-R-2'] !== 0){
+              elseif(isset($marks['DIF-R-2']) && $marks['DIF-R-2'] !== (float)0){
                 $total += $marks['DIF-R-2'];
                 unset($marks['DIF-R-2']);
               }
             }
             else
               $total += $marks['DIF-4'];
-          myDump("DIF-4");
-          myDump($total);
           }
 
           if(isset($marks['DIF-5'])){
             if($marks['DIF-5'] === (float)0){
-              if(isset($marks['DIF-R-1']) && $marks['DIF-R-1'] !== 0 ){
+              if(isset($marks['DIF-R-1']) && $marks['DIF-R-1'] !== (float)0 ){
                 $total += $marks['DIF-R-1'];
                 unset($marks['DIF-R-1']);
               }
-              elseif(isset($marks['DIF-R-2']) && $marks['DIF-R-2'] !== 0){
+              elseif(isset($marks['DIF-R-2']) && $marks['DIF-R-2'] !== (float)0){
                 $total += $marks['DIF-R-2'];
                 unset($marks['DIF-R-2']);
               }
             }
             else
               $total += $marks['DIF-5'];
-          myDump("DIF-5");
-          myDump($total);
           }
 
           if($marks['CEN-1'] === (float)0){
-            if(isset($marks['CEN-R-1']) && $marks['CEN-R-1'] !== 0 ){
+            if(isset($marks['CEN-R-1']) && $marks['CEN-R-1'] !== (float)0 ){
               $total += $marks['CEN-R-1'];
               unset($marks['CEN-R-1']);
             }
-            elseif(isset($marks['CEN-R-2']) && $marks['CEN-R-2'] !== 0){
+            elseif(isset($marks['CEN-R-2']) && $marks['CEN-R-2'] !== (float)0){
               $total += $marks['CEN-R-2'];
               unset($marks['CEN-R-2']);
             }
           }
           else
             $total += $marks['CEN-1'];
-          myDump("CEN-1");
-          myDump($total);
 
           if($marks['CEN-2'] === (float)0){
-            if(isset($marks['CEN-R-1']) && $marks['CEN-R-1'] !== 0 ){
+            if(isset($marks['CEN-R-1']) && $marks['CEN-R-1'] !== (float)0 ){
               $total += $marks['CEN-R-1'];
               unset($marks['CEN-R-1']);
             }
-            elseif(isset($marks['CEN-R-2']) && $marks['CEN-R-2'] !== 0){
+            elseif(isset($marks['CEN-R-2']) && $marks['CEN-R-2'] !== (float)0){
               $total += $marks['CEN-R-2'];
               unset($marks['CEN-R-2']);
             }
           }
           else
             $total += $marks['CEN-2'];
-          myDump("CEN-2");
-          myDump($total);
 
           if($marks['CEN-3'] === (float)0){
-            if(isset($marks['CEN-R-1']) && $marks['CEN-R-1'] !== 0 ){
+            if(isset($marks['CEN-R-1']) && $marks['CEN-R-1'] !== (float)0 ){
               $total += $marks['CEN-R-1'];
               unset($marks['CEN-R-1']);
             }
-            elseif(isset($marks['CEN-R-2']) && $marks['CEN-R-2'] !== 0){
+            elseif(isset($marks['CEN-R-2']) && $marks['CEN-R-2'] !== (float)0){
               $total += $marks['CEN-R-2'];
               unset($marks['CEN-R-2']);
             }
           }
           else
             $total += $marks['CEN-3'];
-          myDump("CEN-3");
-          myDump($total);
 
           if(isset($marks['CEN-4'])){
             if($marks['CEN-4'] === (float)0){
-              if(isset($marks['CEN-R-1']) && $marks['CEN-R-1'] !== 0 ){
+              if(isset($marks['CEN-R-1']) && $marks['CEN-R-1'] !== (float)0 ){
                 $total += $marks['CEN-R-1'];
                 unset($marks['CEN-R-1']);
               }
-              elseif(isset($marks['CEN-R-2']) && $marks['CEN-R-2'] !== 0){
+              elseif(isset($marks['CEN-R-2']) && $marks['CEN-R-2'] !== (float)0){
                 $total += $marks['CEN-R-2'];
                 unset($marks['CEN-R-2']);
               }
             }
             else
               $total += $marks['CEN-4'];
-          myDump("CEN-4");
-          myDump($total);
           }
 
           if(isset($marks['CEN-5'])){
             if($marks['CEN-5'] === (float)0){
-              if(isset($marks['CEN-R-1']) && $marks['CEN-R-1'] !== 0 ){
+              if(isset($marks['CEN-R-1']) && $marks['CEN-R-1'] !== (float)0 ){
                 $total += $marks['CEN-R-1'];
                 unset($marks['CEN-R-1']);
               }
-              elseif(isset($marks['CEN-R-2']) && $marks['CEN-R-2'] !== 0){
+              elseif(isset($marks['CEN-R-2']) && $marks['CEN-R-2'] !== (float)0){
                 $total += $marks['CEN-R-2'];
                 unset($marks['CEN-R-2']);
               }
             }
             else
               $total += $marks['CEN-5'];
-          myDump("CEN-5");
-          myDump($total);
           }
 
           if($marks['ATT-1'] === (float)0){
-            if(isset($marks['ATT-R-1']) && $marks['ATT-R-1'] !== 0 ){
+            if(isset($marks['ATT-R-1']) && $marks['ATT-R-1'] !== (float)0 ){
               $total += $marks['ATT-R-1'];
               unset($marks['ATT-R-1']);
             }
-            elseif(isset($marks['ATT-R-2']) && $marks['ATT-R-2'] !== 0){
+            elseif(isset($marks['ATT-R-2']) && $marks['ATT-R-2'] !== (float)0){
               $total += $marks['ATT-R-2'];
               unset($marks['ATT-R-2']);
             }
           }
           else
             $total += $marks['ATT-1'];
-          myDump("ATT-1");
-          myDump($total);
 
           if(isset($marks['ATT-2'])){
             if($marks['ATT-2'] === (float)0){
-              if(isset($marks['ATT-R-1']) && $marks['ATT-R-1'] !== 0 ){
+              if(isset($marks['ATT-R-1']) && $marks['ATT-R-1'] !== (float)0 ){
                 $total += $marks['ATT-R-1'];
                 unset($marks['ATT-R-1']);
               }
-              elseif(isset($marks['ATT-R-2']) && $marks['ATT-R-2'] !== 0){
+              elseif(isset($marks['ATT-R-2']) && $marks['ATT-R-2'] !== (float)0){
                 $total += $marks['ATT-R-2'];
                 unset($marks['ATT-R-2']);
               }
             }
             else
               $total += $marks['ATT-2'];
-          myDump("ATT-2");
-          myDump($total);
           }
 
           if(isset($marks['ATT-3'])){
             if($marks['ATT-3'] === (float)0){
-              if(isset($marks['ATT-R-1']) && $marks['ATT-R-1'] !== 0 ){
+              if(isset($marks['ATT-R-1']) && $marks['ATT-R-1'] !== (float)0 ){
                 $total += $marks['ATT-R-1'];
                 unset($marks['ATT-R-1']);
               }
-              elseif(isset($marks['ATT-R-2']) && $marks['ATT-R-2'] !== 0){
+              elseif(isset($marks['ATT-R-2']) && $marks['ATT-R-2'] !== (float)0){
                 $total += $marks['ATT-R-2'];
                 unset($marks['ATT-R-2']);
               }
             }
             else
               $total += $marks['ATT-3'];
-          myDump("ATT-3");
-          myDump($total);
           }
         }
-        myDump("TOTAL");
-        myDump($total);
 
         //Update scores, also if the user has no formation ($total will be 0)
         $query = "UPDATE scores
