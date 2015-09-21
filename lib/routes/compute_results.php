a244c66b5036b48eea98610998babb47346071a9
Marco Ardizzone <kwbmm1990@gmail.com>
2015-09-21 18:13:23 +0200

    Fixed query for computing scores

--- lib/routes/compute_results.php
+++ lib/routes/compute_results.php
@@ -50,6 +50,10 @@
                   AND player_mark.MID = (
                     SELECT MAX(MID)
                     FROM match_day
+                    WHERE match_day.end <= '$now')
+                  AND user_formation.MID = (
+                    SELECT MAX(MID)
+                    FROM match_day
                     WHERE match_day.end <= '$now')";
         $result = getResult($app['conn'],$query);
         if($result === false){
@@ -58,12 +62,12 @@
         }
         while (($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) !== null)
           $marks[$row['role']] = (float)$row['mark'];
-
         $notPlayed = array_diff_key($formation,$marks);
 
         foreach ($notPlayed as $role => $mark)
           $marks[$role] = (float)0;
 
+        myDump($marks);
         $total = (float)0;
         if(isset($marks)){ //$marks is set only if user made a formation
           if($marks['POR'] === (float)0 ){
@@ -72,6 +76,8 @@
           }
           else
             $total += $marks['POR'];
+          myDump("POR");
+          myDump($total);
 
           if($marks['DIF-1'] === (float)0){
             if(isset($marks['DIF-R-1']) && $marks['DIF-R-1'] !== 0 ){
@@ -85,6 +91,8 @@
           }
           else
             $total += $marks['DIF-1'];
+          myDump("DIF-1");
+          myDump($total);
 
           if($marks['DIF-2'] === (float)0){
             if(isset($marks['DIF-R-1']) && $marks['DIF-R-1'] !== 0 ){
@@ -98,6 +106,8 @@
           }
           else
             $total += $marks['DIF-2'];
+          myDump("DIF-2");
+          myDump($total);
 
           if($marks['DIF-3'] === (float)0){
             if(isset($marks['DIF-R-1']) && $marks['DIF-R-1'] !== 0 ){
@@ -111,6 +121,8 @@
           }
           else
             $total += $marks['DIF-3'];
+          myDump("DIF-3");
+          myDump($total);
 
           if(isset($marks['DIF-4'])){
             if($marks['DIF-4'] === (float)0){
@@ -125,6 +137,8 @@
             }
             else
               $total += $marks['DIF-4'];
+          myDump("DIF-4");
+          myDump($total);
           }
 
           if(isset($marks['DIF-5'])){
@@ -140,6 +154,8 @@
             }
             else
               $total += $marks['DIF-5'];
+          myDump("DIF-5");
+          myDump($total);
           }
 
           if($marks['CEN-1'] === (float)0){
@@ -154,6 +170,8 @@
           }
           else
             $total += $marks['CEN-1'];
+          myDump("CEN-1");
+          myDump($total);
 
           if($marks['CEN-2'] === (float)0){
             if(isset($marks['CEN-R-1']) && $marks['CEN-R-1'] !== 0 ){
@@ -167,6 +185,8 @@
           }
           else
             $total += $marks['CEN-2'];
+          myDump("CEN-2");
+          myDump($total);
 
           if($marks['CEN-3'] === (float)0){
             if(isset($marks['CEN-R-1']) && $marks['CEN-R-1'] !== 0 ){
@@ -180,6 +200,8 @@
           }
           else
             $total += $marks['CEN-3'];
+          myDump("CEN-3");
+          myDump($total);
 
           if(isset($marks['CEN-4'])){
             if($marks['CEN-4'] === (float)0){
@@ -194,6 +216,8 @@
             }
             else
               $total += $marks['CEN-4'];
+          myDump("CEN-4");
+          myDump($total);
           }
 
           if(isset($marks['CEN-5'])){
@@ -209,6 +233,8 @@
             }
             else
               $total += $marks['CEN-5'];
+          myDump("CEN-5");
+          myDump($total);
           }
 
           if($marks['ATT-1'] === (float)0){
@@ -223,6 +249,8 @@
           }
           else
             $total += $marks['ATT-1'];
+          myDump("ATT-1");
+          myDump($total);
 
           if(isset($marks['ATT-2'])){
             if($marks['ATT-2'] === (float)0){
@@ -237,6 +265,8 @@
             }
             else
               $total += $marks['ATT-2'];
+          myDump("ATT-2");
+          myDump($total);
           }
 
           if(isset($marks['ATT-3'])){
@@ -252,8 +282,12 @@
             }
             else
               $total += $marks['ATT-3'];
+          myDump("ATT-3");
+          myDump($total);
           }
         }
+        myDump("TOTAL");
+        myDump($total);
 
         //Update scores, also if the user has no formation ($total will be 0)
         $query = "UPDATE scores
