ea4dcb0500469be68ec4b7170057dbba6f729b81
Marco Ardizzone <kwbmm1990@gmail.com>
2016-02-02 22:14:18 +0100

    Created userClass

---
+++ lib/includes/userClass.php
@@ -0,0 +1,30 @@
+<?php
+    /**
+     * Vars:
+     *
+     *  + Username (string)
+     *  + Userid (int)
+     *  + Roster (Roster)
+     *  + Points (float)
+     *  + Formations (array<Formation>)
+     *  + Money (int)
+     *
+     * Methods:
+     *
+     *  + getUser
+     *  + getUserID
+     *  + getRoster
+     *  + getPoints
+     *  + getFormation
+     *  + setFormation
+     *  + getMoney
+     */
+    class User
+    {
+        
+        function __construct(argument)
+        {
+            # code...
+        }
+    }
+?>
