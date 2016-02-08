87f245f47be72bf1f8a633facf3d3e9dcdd0cfd2
Marco Ardizzone <kwbmm1990@gmail.com>
2016-02-08 20:07:17 +0100

    Get methods done

--- lib/includes/userClass.php
+++ lib/includes/userClass.php
@@ -11,7 +11,7 @@
      *
      * Methods:
      *
-     *  + getUser
+     *  + getUsername
      *  + getUserID
      *  + getRoster
      *  + getPoints
@@ -21,10 +21,70 @@
      */
     class User
     {
-        
-        function __construct(argument)
-        {
-            # code...
+        private $name=null,$id=null,$roster=null,$points=null,$formations=null,$money==null;
+
+        public function getUsername(){
+            if($this->name == null)
+                $this->setUsername();
+            return $this->name;
         }
+        public function getUserID(){
+            if($this->id==null)
+                $this->setUserID();
+            return $this->id;
+        }
+
+        public function getRoster(){
+            if($this->roster==null)
+                $this->setRoster();
+            return $this->roster;
+        }
+
+        public function getPoints(){
+            if($this->points == null)
+                $this->setPoints();
+            return $this->points;
+        }
+
+        public function getFormations(){
+            if($this->formations == null)
+                $this->setFormations();
+            return $this->formations;
+        }
+
+        public function getFormation($mid){
+            foreach ($this->formations as $formation) {
+                if($formation->getMID() == $mid)
+                    return $formation;
+            }
+            return null;
+        }
+
+        public function getMoney(){
+            if($this->money == null)
+                $this->setMoney();
+            return $this->money;
+        }
+
+        private function setUsername(){
+            //Make query
+        }
+
+        private function setUserID(){
+            //Make query
+        }
+
+        private function setRoster(){
+            //Make query
+        }
+
+        private function setPoints(){
+            //Make query
+        }
+
+        private function setFormations(){
+            //Make query
+        }
+
     }
 ?>
