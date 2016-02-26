eac63bb5f785adf54f1dd15294cb9dde738e6feb
Marco Ardizzone <kwbmm1990@gmail.com>
2016-02-26 21:51:07 +0100

    Indentation correct

--- lib/routes/remember_me.php
+++ lib/routes/remember_me.php
@@ -1,29 +1,29 @@
 <?php 
-  function rememberMe($app){
-    if(isset($_COOKIE['token'])){
-      $current_time = time();
-      $sequence = explode(":", $_COOKIE['token']);
-      $selector = $sequence[0];
-      $token = $sequence[1];
-      $query =  "SELECT token, expires, username
-                FROM  auth_token, user
-                WHERE selector='$selector'
-                AND   auth_token.UID = user.UID";
-      $result = getResult($app['conn'],$query);
-      $row = mysqli_fetch_row($result);
-      if(isset($row)){
-        $sqlTime = $row[1];
-        $expiration = date_create($sqlTime);
-        $expiration = date_format($expiration,'U');
-        if($expiration > $current_time){ //Cookie is still valid
-          $sqlToken = $row[0];
-          $token = hash('sha256',$token);
-          if(hash_equals($sqlToken,$token)){ //Tokens match
-            $username = $row[2];
-            $_SESSION['user'] = $username;
-          }
-        }
-      }
-    }
-  }
+function rememberMe($app){
+	if(isset($_COOKIE['token'])){
+		$current_time = time();
+		$sequence = explode(":", $_COOKIE['token']);
+		$selector = $sequence[0];
+		$token = $sequence[1];
+		$query =  "SELECT token, expires, username
+		FROM  auth_token, user
+		WHERE selector='$selector'
+		AND   auth_token.UID = user.UID";
+		$result = getResult($app['conn'],$query);
+		$row = mysqli_fetch_row($result);
+		if(isset($row)){
+			$sqlTime = $row[1];
+			$expiration = date_create($sqlTime);
+			$expiration = date_format($expiration,'U');
+			if($expiration > $current_time){ //Cookie is still valid
+				$sqlToken = $row[0];
+				$token = hash('sha256',$token);
+				if(hash_equals($sqlToken,$token)){ //Tokens match
+				  	$username = $row[2];
+				  	$_SESSION['user'] = $username;
+				}
+			}
+		}
+	}
+}
 ?>
